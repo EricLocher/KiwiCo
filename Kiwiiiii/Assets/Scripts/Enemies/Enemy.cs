307daf86b520aa96c0c41e340275fa3e089a9514
EricL <eriloc1129@skola.goteborg.se>
2022-04-11 15:25:57 +0200

    Enemy FOV

--- Kiwiiiii/Assets/Scripts/Enemies/Enemy.cs
+++ Kiwiiiii/Assets/Scripts/Enemies/Enemy.cs
@@ -6,9 +6,20 @@ using UnityEngine;
 [Serializable]
 public class Enemy : MonoBehaviour
 {
-    public int Health;
-    float AggroRadius;  
+    [Range(0, 10)]
+    public float aggroRadius;
+    [Range(0,10)]
+    public float fovRadius;
+    [Range(0, 360)]
+    public float fovAngle;
 
+
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+        angleInDegrees += transform.eulerAngles.y;
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
 
 
 
--- Kiwiiiii/Assets/Scripts/Enemies/EnemyEditor.cs
+++ Kiwiiiii/Assets/Scripts/Enemies/EnemyEditor.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 [CustomEditor(typeof(Enemy))]
 public class EnemyEditor : Editor
 {
+    Enemy enemy;
 
+    void OnSceneGUI()
+    {
+        Draw();
+    }
 
+    void Draw()
+    {
+        Handles.color = Color.red;
 
+        Handles.DrawWireArc(enemy.transform.position, Vector3.up, Vector3.forward, 360, enemy.aggroRadius);
+
+        Vector3 viewAngleA = enemy.DirFromAngle(-enemy.fovAngle / 2);
+        Vector3 viewAngleB = enemy.DirFromAngle(enemy.fovAngle / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawWireArc(enemy.transform.position, Vector3.up, viewAngleA, enemy.fovAngle, enemy.fovRadius);
+        Handles.DrawLine(enemy.transform.position, enemy.transform.position + viewAngleA * enemy.fovRadius);
+        Handles.DrawLine(enemy.transform.position, enemy.transform.position + viewAngleB * enemy.fovRadius);
+
+    }
+
+    void OnEnable()
+    {
+        enemy = (Enemy)target;
+    }
 
 }
