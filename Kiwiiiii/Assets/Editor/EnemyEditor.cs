506fba97dbd43cae25f5944e04f964c720a28be3
EricLocher <eriloc1129@skola.goteborg.se>
2022-04-14 13:43:06 +0200

    Changes to Enemy

--- Kiwiiiii/Assets/Editor/EnemyEditor.cs
+++ Kiwiiiii/Assets/Editor/EnemyEditor.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(Enemy))]
+[CustomEditor(typeof(Enemy), true)]
 public class EnemyEditor : Editor
 {
     Enemy enemy;
@@ -14,10 +14,13 @@ public class EnemyEditor : Editor
     void Draw()
     {
         Handles.color = Color.red;
+        Handles.DrawWireArc(enemy.transform.position, Vector3.up, Vector3.forward, 360, enemy.attackDistance);
 
+        Handles.color = new Color(1, 0.65f, 0);
         Handles.DrawWireArc(enemy.transform.position, Vector3.up, Vector3.forward, 360, enemy.aggroRadius);
         Vector3 viewAngleA = enemy.DirFromAngle(-enemy.fovAngle / 2);
         Vector3 viewAngleB = enemy.DirFromAngle(enemy.fovAngle / 2);
+
         Handles.color = Color.yellow;
         Handles.DrawWireArc(enemy.transform.position, Vector3.up, viewAngleA, enemy.fovAngle, enemy.fovRadius);
         Handles.DrawLine(enemy.transform.position, enemy.transform.position + viewAngleA * enemy.fovRadius);
--- Kiwiiiii/Assets/Scripts/Enemies/Enemy.cs
+++ Kiwiiiii/Assets/Scripts/Enemies/Enemy.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [Serializable, RequireComponent(typeof(NavMeshAgent))]
-public class Enemy : FOV
+public abstract class Enemy : FOV
 {
+    [Header("Target")]
     [SerializeField] Transform target;
+    [Header("Stats")]
+    [SerializeField] float health;
+    [SerializeField] float moveSpeed;
+    [Range(0, 10)] public float attackDistance;
+
     EnemyStates state = EnemyStates.Idle;
     NavMeshAgent navMeshAgent;
 
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();    
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = attackDistance;
     }
 
     void Update()
     {
-        if (TargetInView(target)) {
+        Track();
+    }
+
+    protected abstract void Attack();
+    protected void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0) {
+            health = 0;
+        }
+    }
+
+    protected virtual void Track()
+    {
+        if (Vector3.Distance(transform.position, target.position) < attackDistance) {
+            Attack();
+        }
+        else if (TargetInView(target)) {
             navMeshAgent.SetDestination(target.position);
         }
     }
