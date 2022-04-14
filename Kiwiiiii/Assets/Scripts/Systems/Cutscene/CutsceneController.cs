d744d1398aea80348fbafc0a4ab71a11d8c8a3a3
Niklas Jörgensen <niklio15csgo@hotmail.com>
2022-04-14 23:54:17 +0200

    cutscene test

---
+++ Kiwiiiii/Assets/Scripts/Systems/Cutscene/CutsceneController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CutsceneController : MonoBehaviour
+{
+    public Queue<string> sentances;
+    public TextMeshProUGUI dialogueText;
+    public Animator dialogueBox;
+    public string sentance;
+
+    void Start()
+    {
+        sentances = new Queue<string>();
+
+        StartCoroutine("StartText");
+    }
+
+    IEnumerator StartText()
+    {
+        yield return new WaitForSeconds(4f);
+        dialogueBox.SetBool("IsOpen", true);
+        StartCoroutine(TypeSentance(sentance));
+    }
+
+    IEnumerator TypeSentance(string sentance)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentance.ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            yield return null;
+        }
+    }
+}
