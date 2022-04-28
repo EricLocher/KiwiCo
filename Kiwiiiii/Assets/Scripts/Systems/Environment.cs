1fa250778ad2dc36cecac7b454fe344108400cfb
Niklsa <niklio15csgo@hotmail.com>
2022-04-28 09:50:39 +0200

    robert tvinga mig

--- Kiwiiiii/Assets/Scripts/Systems/Environment.cs
+++ Kiwiiiii/Assets/Scripts/Systems/Environment.cs
@@ -11,6 +11,17 @@ public class Environment : MonoBehaviour
     };
     [SerializeField]
     Object type;
+    enum DirectionalBoost
+    {
+        Forward,
+        Back,
+        Left,
+        Right,
+        Up,
+        Down
+    };
+    [SerializeField]
+    DirectionalBoost DirectionBoost;
 
     [SerializeField, Range(0f, 1000f)]
     float force = 500f;
@@ -19,15 +30,13 @@ public class Environment : MonoBehaviour
     ForceMode forceMode;
 
     Vector3 direction;
-
-    [SerializeField]
-    Vector3 DirectionalBoostPad = Vector3.left;
+    Vector3 DirectionBoostPad;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.isTrigger) { return; }
 
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.CompareTag("Player"))
         {
             if(type == Object.JumpPad)
                 direction = transform.TransformDirection(Vector3.up * force);
@@ -36,7 +45,27 @@ public class Environment : MonoBehaviour
                 direction = transform.InverseTransformDirection(other.gameObject.GetComponent<Rigidbody>().velocity * force);
 
             if(type == Object.DirectionalBoostPad)
-                direction = direction = transform.TransformDirection(DirectionalBoostPad * force);
+            {
+                if (DirectionBoost == DirectionalBoost.Forward)
+                    DirectionBoostPad = Vector3.forward;
+
+                if (DirectionBoost == DirectionalBoost.Back)
+                    DirectionBoostPad = Vector3.back;
+
+                if (DirectionBoost == DirectionalBoost.Left)
+                    DirectionBoostPad = Vector3.left;
+
+                if (DirectionBoost == DirectionalBoost.Right)
+                    DirectionBoostPad = Vector3.right;
+
+                if (DirectionBoost == DirectionalBoost.Up)
+                    DirectionBoostPad = Vector3.up;
+
+                if (DirectionBoost == DirectionalBoost.Down)
+                    DirectionBoostPad = Vector3.down;
+
+                direction = transform.TransformDirection(DirectionBoostPad * force);
+            }
 
             other.gameObject.GetComponent<Rigidbody>().AddForce(direction, forceMode);
         }
