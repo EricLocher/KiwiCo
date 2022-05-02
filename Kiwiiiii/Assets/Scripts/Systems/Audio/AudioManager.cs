c7852b7aec1bfc55b448aedb6a3c291ebded3252
Niklas Jörgensen <niklio15csgo@hotmail.com>
2022-05-02 11:24:45 +0200

    more advanced audio

---
+++ Kiwiiiii/Assets/Scripts/Systems/Audio/AudioManager.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    [HideInInspector]
+    public float menuVolume;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            sound.audio = gameObject.AddComponent<AudioSource>();
+            sound.audio.clip = sound.clip;
+            sound.audio.volume = sound.volume;
+            sound.audio.loop = sound.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        s.audio.Play();
+    }
+
+    public void PlayOnce(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        s.audio.PlayOneShot(s.clip);
+    }
+
+    public void PauseAllSound()
+    {
+        foreach (Sound sound in sounds)
+        {
+            sound.audio.Pause();
+        }
+    }
+}
---
+++ Kiwiiiii/Assets/Scripts/Systems/Audio/Sound.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [HideInInspector]
+    public AudioSource audio;
+
+    public bool loop;
+}
