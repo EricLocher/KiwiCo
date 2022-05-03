bd7e39040783a4cc9fe248cf2371cd5bacda4d96
Niklsa <niklio15csgo@hotmail.com>
2022-05-03 15:21:44 +0200

    enemy random circle patrolling

--- Kiwiiiii/Assets/Scripts/Enemies/Editor/PatrolEditor.cs
+++ Kiwiiiii/Assets/Scripts/Enemies/Editor/PatrolEditor.cs
@@ -12,33 +12,63 @@ public class PatrolEditor : Editor
     {
         base.OnInspectorGUI();
 
-        if (GUILayout.Button("Show Spots")) {
-            showSpots = !showSpots;
-        }
-
-        if(!showSpots) { return; }
-
-        if (GUILayout.Button("Add Spot")) {
-            Undo.RecordObject(target, "Added New Spot");
-            patrolSpots.AddSpot(new Vector3(0, 10, 0));
-        }
-
-        if (GUILayout.Button("Remove Spot")) {
-            Undo.RecordObject(target, "Removed Spot");
-            patrolSpots.RemoveSpot(lastMovedIndex);
-            lastMovedIndex = 0;
-        }
-
-        if (GUILayout.Button("Raycast Spot to ground")) {
-            Undo.RecordObject(target, "Grounded Spot");
-            patrolSpots.RayTraceToGround(lastMovedIndex);
+        if (GUILayout.Button("Use Circle"))
+        {
+            Undo.RecordObject(target, "Updated Behaviour");
+            patrolSpots.circlePos = patrolSpots.transform.position;
+            patrolSpots.circle = !patrolSpots.circle;
         }
 
 
-        if (GUILayout.Button("Raycast All to ground")) {
-            Undo.RecordObject(target, "Grounded All Spots");
-            for (int i = 0; i < patrolSpots.spots.Count; i++) {
-                patrolSpots.RayTraceToGround(i);
+        if (!patrolSpots.circle)
+        {
+
+            if (GUILayout.Button("Show Spots"))
+            {
+                showSpots = !showSpots;
+            }
+
+            if (showSpots)
+            {
+                if (GUILayout.Button("Add Spot"))
+                {
+                    Undo.RecordObject(target, "Added New Spot");
+                    patrolSpots.AddSpot(new Vector3(0, 10, 0));
+                }
+
+                if (GUILayout.Button("Remove Spot"))
+                {
+                    Undo.RecordObject(target, "Removed Spot");
+                    patrolSpots.RemoveSpot(lastMovedIndex);
+                    lastMovedIndex = 0;
+                }
+
+                if (GUILayout.Button("Raycast Spot to ground"))
+                {
+                    Undo.RecordObject(target, "Grounded Spot");
+                    patrolSpots.RayTraceToGround(lastMovedIndex);
+                }
+
+
+                if (GUILayout.Button("Raycast All to ground"))
+                {
+                    Undo.RecordObject(target, "Grounded All Spots");
+                    for (int i = 0; i < patrolSpots.spots.Count; i++)
+                    {
+                        patrolSpots.RayTraceToGround(i);
+                    }
+                }
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Patrol Radius", EditorStyles.boldLabel);
+            float sliderVal = EditorGUILayout.Slider(patrolSpots.circleRadius, 10, 100);
+            if (patrolSpots.circleRadius != sliderVal)
+            {
+                Undo.RecordObject(patrolSpots, "Updated Radius");
+
+                patrolSpots.circleRadius = sliderVal;
             }
         }
     }
@@ -50,12 +80,21 @@ public class PatrolEditor : Editor
 
     void Draw()
     {
+        if (patrolSpots.circle)
+        {
+            Handles.color = Color.red;
+            Handles.DrawWireArc(patrolSpots.circlePos, Vector3.up, Vector3.forward, 360, patrolSpots.circleRadius);
+            return;
+        }
+
+
         if (!showSpots) { return; }
 
         if (patrolSpots.spots.Count == 0) { return; }
 
 
-        for (int i = 0; i < patrolSpots.spots.Count; i++) {
+        for (int i = 0; i < patrolSpots.spots.Count; i++)
+        {
             Handles.color = Color.yellow;
             Handles.DrawLine(patrolSpots.transform.position, patrolSpots[i]);
 
@@ -63,7 +102,8 @@ public class PatrolEditor : Editor
             else { Handles.color = Color.green; }
             Vector3 newPos = Handles.FreeMoveHandle(patrolSpots[i], Quaternion.identity, .5f, Vector2.zero, Handles.CylinderHandleCap);
 
-            if (patrolSpots[i] != newPos) {
+            if (patrolSpots[i] != newPos)
+            {
                 Undo.RecordObject(patrolSpots, "Move Spot");
                 patrolSpots.MoveSpot(i, newPos);
                 lastMovedIndex = i;
