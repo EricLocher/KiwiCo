9cd313f6ae1163b028753b3f3b9687f0590d8962
NeoLjusica <jessiicaceder@hotmail.com>
2022-05-05 13:28:39 +0200

    hEJ

---
+++ Kiwiiiii/Assets/Scripts/Enemies/Attacks/SpearAttack.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearAttack : EnemyAttack
+{
+    public override void ActiveAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void EnterAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExitAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
