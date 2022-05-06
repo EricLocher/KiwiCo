c48674363a41fde22a550657a752c40b7c0a76ec
Niklsa <niklio15csgo@hotmail.com>
2022-05-06 12:54:05 +0200

    new ui

---
+++ Kiwiiiii/Assets/Scripts/Player/HealthUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] Image fillImage;
+    [SerializeField] PlayerController player;
+
+
+    void Update()
+    {
+        fillImage.fillAmount = (player.stats.health / 10);
+    }
+}
