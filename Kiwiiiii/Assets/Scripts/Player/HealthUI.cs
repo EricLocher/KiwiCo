e956119b429a07e66604de8ec4bee9c7e61f6f42
Niklsa <niklio15csgo@hotmail.com>
2022-05-06 13:12:40 +0200

    new ui working

--- Kiwiiiii/Assets/Scripts/Player/HealthUI.cs
+++ Kiwiiiii/Assets/Scripts/Player/HealthUI.cs
@@ -6,9 +6,10 @@ public class HealthUI : MonoBehaviour
     [SerializeField] Image fillImage;
     [SerializeField] PlayerController player;
 
-
     void Update()
     {
-        fillImage.fillAmount = (player.stats.health / 10);
+        fillImage.fillAmount = (player.stats.health / 100f);
+
+        Debug.Log(player.stats.health);
     }
 }
