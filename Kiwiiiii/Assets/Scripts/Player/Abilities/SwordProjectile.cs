cab1926b385f8790ca6b3e0527f4f0c582b1372c
EricLocher <eriloc1129@skola.goteborg.se>
2022-05-06 23:18:36 +0200

    Working on new Ability

---
+++ Kiwiiiii/Assets/Scripts/Player/Abilities/SwordProjectile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SwordProjectile", menuName = "Utilities/Abilities/SwordProjectile")]
+public class SwordProjectile : Ability
+{
+    public override void DoAbility()
+    {
+        
+    }
+}
