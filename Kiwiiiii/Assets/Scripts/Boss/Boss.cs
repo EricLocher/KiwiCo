91b93d19763634af6b586fe3b27277e6fb120557
EricL <eriloc1129@skola.goteborg.se>
2022-05-16 15:49:33 +0200

    Boss

--- Kiwiiiii/Assets/Scripts/Boss/Boss.cs
+++ Kiwiiiii/Assets/Scripts/Boss/Boss.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Boss : Character
 {
-    [SerializeField] List<Enemy> enemies;
+    public List<Enemy> enemies;
+    public BossHealthBar healthBar;
     [HideInInspector] public Transform target;
     [HideInInspector] public PatrolSpots spawnAreas;
-    public SOBossStats stats { get { return (SOBossStats)stats; } }
+    public SOBossStats stats { get { return stats; } }
 
     BossStateMachine stateMachine;
 
@@ -15,7 +15,16 @@ public class Boss : Character
     {
         target = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
         spawnAreas = GetComponent<PatrolSpots>();
+
+        stateMachine = new BossStateMachine();
+
+        stateMachine.RegisterState(BossStates.Init, new BossInitState(this, stateMachine));
+        stateMachine.RegisterState(BossStates.Shield, new ShieldState(this, stateMachine));
+        stateMachine.RegisterState(BossStates.Spawning, new SpawnState(this, stateMachine));
         stateMachine.boss = this;
+        stateMachine.ChangeState(BossStates.Init);
+
+        healthBar.stats = stats;
     }
 
     void Update()
---
+++ Kiwiiiii/Assets/Scripts/Boss/States/ShieldState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ShieldState : BossState
+{
+    public ShieldState(Boss agent, BossStateMachine stateMachine) : base(agent, stateMachine) { }
+    public override BossStates GetId() => BossStates.Shield;
+
+    float test = 5;
+    float elapsedTime = 0; 
+
+    public override void EnterState()
+    {
+        Debug.Log(GetId());
+    }
+
+    public override void Update(float dt = 0)
+    {
+        elapsedTime += dt;
+        if(elapsedTime > test) {
+            stateMachine.ChangeState(BossStates.Spawning);
+        }
+    }
+
+    public override void ExitState()
+    {
+
+    }
+
+}
+
