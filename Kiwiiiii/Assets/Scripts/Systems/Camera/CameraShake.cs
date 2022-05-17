8664f709aea6835aa59be04cababd26f00d8ecaa
Niklsa <niklio15csgo@hotmail.com>
2022-05-17 13:20:51 +0200

    working on camerashake

---
+++ Kiwiiiii/Assets/Scripts/Systems/Camera/CameraShake.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake instance;
+
+    Vector3 _originalPos;
+    float _timeAtCurrentFrame;
+    float _timeAtLastFrame;
+    float _fakeDelta;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        _timeAtCurrentFrame = Time.realtimeSinceStartup;
+        _fakeDelta = _timeAtCurrentFrame - _timeAtLastFrame;
+        _timeAtLastFrame = _timeAtCurrentFrame;
+    }
+
+    public static void Shake(float duration, float amount)
+    {
+        instance._originalPos = instance.gameObject.transform.localPosition;
+        instance.StopAllCoroutines();
+        instance.StartCoroutine(instance.cShake(duration, amount));
+    }
+
+    public IEnumerator cShake(float duration, float amount)
+    {
+        float endTime = Time.time + duration;
+
+        while (duration > 0)
+        {
+            transform.localPosition = _originalPos + Random.insideUnitSphere * amount;
+
+            duration -= _fakeDelta;
+
+            yield return null;
+        }
+
+        transform.localPosition = _originalPos;
+    }
+}
