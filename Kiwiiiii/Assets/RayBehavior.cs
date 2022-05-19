b14b0ad20e10516c4d75c9a9fd29ad60a5f0fc25
PepperRoni <90191976+PepperRoni@users.noreply.github.com>
2022-05-19 11:17:48 +0200

    Ray does damage

--- Kiwiiiii/Assets/RayBehavior.cs
+++ Kiwiiiii/Assets/RayBehavior.cs
@@ -2,23 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileBehavior : MonoBehaviour
+public class RayBehavior : MonoBehaviour
 {
-    [SerializeField] public float speed;
     [SerializeField] public float damage;
-    Rigidbody rb;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.AddForce(-transform.forward * speed, ForceMode.Impulse);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Character"))
         {
             other.gameObject.transform.parent.GetComponent<PlayerController>().TakeDamage(damage);
-            damage = 0;
         }
     }
 }
