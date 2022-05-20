4a96aadb7ce6b207845ae982442805d3a17d51b9
Niklsa <niklio15csgo@hotmail.com>
2022-05-20 09:12:25 +0200

    updated enemy prefabs and added music loader script

---
+++ Kiwiiiii/Assets/Scripts/Systems/MusicManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    int buildIndex = 0;
+    void OnEnable()
+    {
+        buildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (buildIndex == 0 || buildIndex == 4)
+        {
+            AudioManager.instance.PauseAllSound();
+            AudioManager.instance.Play("Menu Music");
+        }
+        AudioManager.instance.PauseAllSound();
+        AudioManager.instance.Play("Game Music");
+    }
+}
