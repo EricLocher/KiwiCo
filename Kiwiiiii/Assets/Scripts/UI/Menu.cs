c97ad6e4174ea198aeea7ece149bee8d53b8bb68
Niklsa <niklio15csgo@hotmail.com>
2022-05-25 12:39:53 +0200

    menu logic completely done now

--- Kiwiiiii/Assets/Scripts/UI/Menu.cs
+++ Kiwiiiii/Assets/Scripts/UI/Menu.cs
@@ -1,6 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class Menu : MonoBehaviour
 {
